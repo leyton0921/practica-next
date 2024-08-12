074eb847cb9a7e37277bd156ee0723219969e35c
Samuel Rodriguez <samuelrodriguez795@gmail.com>
2024-08-12 08:09:41 -0500

    paginas

---
+++ app/link1/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Home1() {
+    return (
+      <main >
+        <h1>holaaaa</h1>
+        
+        <Link href="/link2">siguiente</Link>
+      </main>
+    );
+  }
---
+++ app/link2/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Home2() {
+    return (
+      <main >
+        <h1>adios</h1>
+
+      <Link href="/link1">atras</Link>
+
+
+  
+      </main>
+    );
+  }
